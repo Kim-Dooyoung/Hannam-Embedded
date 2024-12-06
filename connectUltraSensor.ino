1066e61e631688205b3d0f1875ccdb7f6d8bdbd4
Kim-Dooyoung <152779786+Kim-Dooyoung@users.noreply.github.com>
2024-12-06 14:09:23 +0900

    Update connectUltraSensor.ino

--- connectUltraSensor.ino
+++ connectUltraSensor.ino
@@ -9,6 +9,9 @@ void setup() {
 
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
+
+  myservo.attach(2);
+  myservo.write(0);
 }
 
 void loop() {
@@ -27,11 +30,26 @@ void loop() {
   duration = pulseIn (ECHO, HIGH);
   distance = duration * 17 / 1000; 
 
+// log 찍기
   Serial.println(duration );
   Serial.print("\nDIstance : ");
 
   Serial.print(distance);
   Serial.println(" Cm");
 
+  // delay(1000);
+
+  if (distance <= 20) {
+    // 모터 열기
+    // n 초 후에 자동으로 닫히게
+    Serial.println("Object detected. Opening servo...");
+    myservo.write(90); 
+    delay(5000);       // 3초 대기
+    Serial.println("Closing servo...");
+    myservo.write(0);  
+  }
+
   delay(1000);
+
+
 }
